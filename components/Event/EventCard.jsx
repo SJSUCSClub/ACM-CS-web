925cc0aa2a51429cce9d57f3a7db7e606dadfae5
Trique Nguyen <ngngoctridung123@gmail.com>
2023-10-13 02:51:54 -0700

    done blogPage. Need to implement blogPost

--- components/Event/EventCard.jsx
+++ components/Event/EventCard.jsx
@@ -7,7 +7,7 @@ const EventCard = ({ title, description, schedule, location, company, website })
   return (
     <div 
       
-    className="transition duration-100 ease-linear hover:shadow-2xl w-[20rem] sm:w-[18rem] h-auto bg-neutral-200 px-6 py-4 rounded-lg flex flex-col gap-2 drop-shadow-lg">
+    className="border transition duration-100 ease-linear hover:shadow-lg w-[20rem] sm:w-[18rem] h-auto bg-neutral-200 px-6 py-4 rounded-lg flex flex-col gap-2 shadow-md">
       <h1 className="text-2xl sm:text-xl font-bold">{title}</h1>
       {/* <p>{description}</p> */}
       <div className="flex flex-col text-[16px] sm:text-sm font-semibold">
---
+++ server/utils/blog-helper.js
@@ -0,0 +1,81 @@
+import { discussionDetailGql, discussionGql } from './gql'
+
+
+const API_URL = 'https://api.github.com/graphql'
+const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN
+const DISCUSSION_CATEGORY_ID = process.env.DISCUSSION_CATEGORY_ID
+
+export async function getBlogs() {
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer `,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: discussionGql('DIC_kwDOKP9wN84CaFwd') }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err))
+  const discussions = res.data.repository.discussions.nodes
+  const posts = discussions.map((discussion) => {
+    const { title, discussionUrl, number, bodyHTML, bodyText, createdAt, lastEditedAt, author, labels } = discussion
+    const url = `/blog/${number}`
+    const authorUrl = author.url
+    const authorName = author.login
+    const authorAvatar = author.avatarUrl
+    const tags = labels.nodes.map((label) => label.name)
+
+    console.log(tags)
+
+    const post = {
+      number,
+      url,
+      discussionUrl,
+      title,
+      bodyHTML,
+      bodyText,
+      tags,
+      createdAt,
+      lastEditedAt,
+      author: {
+        url: authorUrl,
+        name: authorName,
+        avatar: authorAvatar
+      }
+    }
+
+    return post
+  })
+  return posts
+}
+
+export async function getBlogDetail(id) {
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer `
+    },
+    body: JSON.stringify({ query: discussionDetailGql('DIC_kwDOKP9wN84CaFwd') })
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err))
+
+  const discussion = res.data.repository.discussion
+
+  const {
+    author: { url, login, avatarUrl },
+    createdAt,
+    title: title,
+    bodyHTML,
+  } = discussion
+
+  const detail = {
+    author: { url, name: login, avatar: avatarUrl },
+    createdAt,
+    title,
+    bodyHTML,
+  }
+  return detail
+}
+
