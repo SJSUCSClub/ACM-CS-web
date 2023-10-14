2490915734b524d36b714ca47371adaed9035948
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2023-10-14 04:25:49 -0700

    Extracted components to create-event-form

--- app/admin/create-event/page.jsx
+++ app/admin/create-event/page.jsx
@@ -1,41 +1,10 @@
-import TextInput from "@/components/Admin/TextInput";
+import CreateEventForm from "@/components/Admin/Create-Event/create-event-form";
 import React from "react";
-
-// TODO: Add remaining inputs, form validation, and submit button
-/*
-Event:
-Image
-Title
-Description
-
-Logistics:
-Date
-Location
-
-Attendance:
-Max Attendees
-Paid Member Only or Not (dropdown)
-Type of Event (dropdown)
-*/
 
 const page = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold drop-shadow-lg">
-                Create a New Event
-            </h1>
-            <TextInput
-                id="eventTitle"
-                label="Event Title"
-                error="Wrong input."
-                placeholder="Enter event title"
-            />
-            <TextInput
-                id="eventTitle"
-                label="Event Title"
-                error=""
-                placeholder="Enter event title"
-            />
+            <CreateEventForm />
         </div>
     );
 };
