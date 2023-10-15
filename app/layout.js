7ddaaea44b929d522b00d38124be991d1ebc2bac
aishse <email.friendme@gmail.com>
2023-10-15 14:03:31 -0700

    fixed favicon rendering

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,37 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import Logo from '@/public/icons/logo.png'
-const inter = Inter({ subsets: ['latin'] })
-import { NextAuthProvider } from './Provider'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Logo from "@/public/icons/logo.png";
+const inter = Inter({ subsets: ["latin"] });
+import { NextAuthProvider } from "./Provider";
 
 export const metadata = {
-  title: 'ACM @ SJSU',
-  description: 'ACM Chapter at San Jose State University',
-  icon: { Logo },
-}
+  title: "ACM @ SJSU",
+  description: "ACM Chapter at San Jose State University",
+  // icon: { Logo },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="@/public/icons/logo.png" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
       </head>
-      <body className={`${inter.className} flex flex-col justify-center items-center bg-[#f5f8f1] gap-12`}>
+      <body
+        className={`${inter.className} flex flex-col justify-center items-center bg-[#f5f8f1] gap-12`}
+      >
         <NextAuthProvider>
           <Navbar />
           {children}
           <Footer />
         </NextAuthProvider>
       </body>
-
     </html>
-  )
+  );
 }
