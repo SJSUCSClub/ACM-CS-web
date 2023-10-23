00af1d98eeb7b989037edd006f77cf5795182101
Ali Fayed <alifayed.h@gmail.com>
2023-10-23 15:54:12 -0700

    Added try and catch to requests

--- app/api/event/route.js
+++ app/api/event/route.js
@@ -4,27 +4,37 @@ import { NextResponse } from 'next/server';
 
 export async function GET(res) {
     await connectDB();
-    const allEvents = await Event.find();
-    return NextResponse.json({events: allEvents});
+    try {
+        const allEvents = await Event.find();
+        return NextResponse.json({ allEvents }, { status: 200 });
+    } catch(error) {
+        console.log("/api/event/GET: ", error.message);
+        return NextResponse.json({ error: error.message }, { status: 400 });
+    }
 }
 
 export async function POST(req) {
     const eventInfo = await req.json();
     await connectDB();
 
-    const event = await Event.findOne({title: eventInfo.title});
-    if(!event)
-    {
-        await Event.create({
-            title: eventInfo.title,
-            description: eventInfo.description,
-            date: eventInfo.date,
-            location: eventInfo.location,
-            maxAttendees: eventInfo.maxAttendees,
-            paidMemberOnly: eventInfo.paidMemberOnly,
-            type: eventInfo.type
-        });
+    try {
+        const event = await Event.findOne({title: eventInfo.title});
+        if(!event)
+        {
+            await Event.create({
+                title: eventInfo.title,
+                description: eventInfo.description,
+                date: eventInfo.date,
+                location: eventInfo.location,
+                image: eventInfo.image,
+                maxAttendees: eventInfo.maxAttendees,
+                paidMemberOnly: eventInfo.paidMemberOnly,
+                type: eventInfo.type
+            });
+        }
+        return NextResponse.json({ event }, { status: 200 });
+    } catch(error) {
+        console.log("/api/event/POST: ", error.message);
+        return NextResponse.json({ error: error.message }, { status: 400 });
     }
-
-    return NextResponse.json({message: 'Event Created'});
 }
