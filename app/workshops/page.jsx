d1e720845a62f1f85842be6710cc85695b2ffa40
kevintsoii <kevin.tsoi44@gmail.com>
2023-10-24 11:59:49 -0700

    add workshops

---
+++ app/workshops/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkshopsPage = () => {
+  return (
+    <div className="h-screen">
+      <h1>this is workshop</h1>
+    </div>
+  );
+};
+
+export default WorkshopsPage;
