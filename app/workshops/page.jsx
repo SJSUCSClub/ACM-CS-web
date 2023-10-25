9a14ff4b6b4f8d9f4c05a9f8c511b9cb84db34c2
Marvin Zhai <marvinzhai@Marvins-Air.attlocal.net>
2023-10-24 20:38:00 -0700

    added animation

--- app/workshops/page.jsx
+++ app/workshops/page.jsx
@@ -1,7 +1,13 @@
+"use client"
 import React from "react";
 import WorkshopCard from "@/components/Workshop/WorkshopCard";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const WorkshopsPage = () => {
+
+  const [visibleItems,setVisibleItems] = useState([]);
+
   const events = [
     {
       title: "React Workshop",
@@ -18,6 +24,17 @@ const WorkshopsPage = () => {
     },
   ];
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (visibleItems.length < events.length) {
+        setVisibleItems((prevVisible) => [...prevVisible, prevVisible.length]);
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 500);
+    return () => clearInterval(intervalId);
+  }, [visibleItems]);
+
   return (
     <div className="xl:h-[80vh] flex flex-col gap-12 sm:gap-2 justify-center items-center max-w-[1440px]">
       <div className="flex flex-col gap-6 sm:justify-center sm:items-center">
@@ -29,7 +46,19 @@ const WorkshopsPage = () => {
           workshops!
         </h2>
         {events.map((event, index) => (
-          <WorkshopCard key={index} {...event} />
+          <motion.div 
+            key={index}
+            initial={{ opacity: 0, y:0 }}
+            animate={
+              visibleItems.includes(index)
+                ? { opacity: 1, y: 0 }
+                : { opacity: 0, y: -50 }
+            }
+            transition = {{ duration: 0.5, delay: index * 0.1 }}
+          >
+
+            <WorkshopCard key={index} {...event} />
+            </motion.div>
         ))}
       </div>
     </div>
