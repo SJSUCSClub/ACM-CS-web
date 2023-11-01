ebf5e11fb35269b7aaa557ac41e3a3f77ac3dd0e
Trique Nguyen <ngngoctridung123@gmail.com>
2023-10-31 19:33:23 -0700

    fixed fetching events

--- components/Event/EventFeed.jsx
+++ components/Event/EventFeed.jsx
@@ -1,53 +1,32 @@
+"use client"
+
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EventCard from "./EventCard";
 
-const events = [
-  {
-    title: "MESA Club Fair",
-    description: "Come to learn more about us at the Club Fair!",
-    schedule: "Sept 13, 11 AM - 2 PM",
-    location: "7th Street Plaza",
-  },
-  {
-    title: "Kohl's Info Session",
-    description: "Come to learn more about us at the Club Fair!",
-    schedule: "Sept 14, 1 PM - 2 PM",
-    location: "MQH 227",
-  },
-  {
-    title: "CS Club Fair 2",
-    description: "Come to learn more about us at the Club Fair!",
-    schedule: "Sept 15, 11 AM - 2 PM",
-    location: "MQH 226 - 227",
-  },
-  {
-    title: "CS Club Fair 2",
-    description: "Come to learn more about us at the Club Fair!",
-    schedule: "Sept 15, 11 AM - 2 PM",
-    location: "MQH 226 - 227",
-  },
-];
-
-const getEvents = async () => {
-  try {
-    const rest = await fetch("http://localhost:3000/api/event", {
-      cache: "no-cache",
-    });
-
-    if (!rest.ok) {
-      throw new Error(rest);
-    }
-
-    const json = await rest.json();
-    console.log(json);
-    return json;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export default function EventFeed() {
+  const [events, setEvents] = useState([]);
+
+  const getEvents = async () => {
+    try {
+      const rest = await fetch("http://localhost:3000/api/event")
+  
+      if (!rest.ok) {
+        throw new Error(rest);
+      }
+  
+      const json = await rest.json();
+      setEvents(json.events);
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, [events]);
+
   return (
     <>
       <div className="grid grid-cols-3 gap-5">
