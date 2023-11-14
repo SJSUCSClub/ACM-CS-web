0dd82d1395f1684fbe295ae9b9c0b55756d47721
aishse <email.friendme@gmail.com>
2023-11-13 17:59:22 -0800

    SWR support

--- components/Event/EventFeed.jsx
+++ components/Event/EventFeed.jsx
@@ -3,11 +3,19 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import EventCard from "./EventCard";
+import useSWR from "swr";
 
 export default function EventFeed() {
-  const [events, setEvents] = useState([]);
-
+  const fetcher = async () => {
+    const rest = await fetch("http://localhost:3000/api/event");
+    const json = await rest.json();
+    return json.events;
+  };
+  /*
+  
+  
   const getEvents = async () => {
+    
     try {
       const rest = await fetch("http://localhost:3000/api/event");
 
@@ -23,14 +31,20 @@ export default function EventFeed() {
     }
   };
 
-  useEffect(() => {
-    getEvents();
-  }, [events]);
+  useEffect(() => {getEvents()}, [events]);
+*/
 
+  const { data, isLoading, error } = useSWR(
+    "http://localhost:3000/api/event",
+    fetcher
+  );
+
+  if (error) return <h1>Something went wrong!</h1>;
+  if (isLoading) return <h1>Loading events...</h1>;
   return (
     <>
       <div className="grid grid-cols-3 gap-5">
-        {events.map((event) => (
+        {data.map((event) => (
           <EventCard key={event.id} {...event} />
         ))}
       </div>
